6c7785c88123d6b59330d92e35a924f85e9ee990
Cyprus Basio <donylcyprus@gmail.com>
2025-09-29 16:28:32 +0800

    PLSSS

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,15 +16,15 @@
         private void InitializeComponent()
         {
             UserTb = new TextBox();
-            PasswordTb = new TextBox();
             EmailTb = new TextBox();
+            PasswordTb = new TextBox();
             SignupBtn = new Button();
             linkLabel1 = new LinkLabel();
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
             label4 = new Label();
+            passregTB = new Label();
+            label2 = new Label();
+            label1 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -39,31 +39,34 @@
             UserTb.PlaceholderText = "Username";
             UserTb.Size = new Size(176, 28);
             UserTb.TabIndex = 0;
-            // 
-            // PasswordTb
-            // 
-            PasswordTb.Anchor = AnchorStyles.None;
-            PasswordTb.BackColor = Color.NavajoWhite;
-            PasswordTb.BorderStyle = BorderStyle.None;
-            PasswordTb.Location = new Point(112, 300);
-            PasswordTb.Multiline = true;
-            PasswordTb.Name = "PasswordTb";
-            PasswordTb.PlaceholderText = "Email";
-            PasswordTb.Size = new Size(176, 30);
-            PasswordTb.TabIndex = 1;
-            PasswordTb.UseSystemPasswordChar = true;
+            UserTb.TextChanged += UserTb_TextChanged;
             // 
             // EmailTb
             // 
             EmailTb.Anchor = AnchorStyles.None;
             EmailTb.BackColor = Color.NavajoWhite;
             EmailTb.BorderStyle = BorderStyle.None;
-            EmailTb.Location = new Point(111, 254);
+            EmailTb.Location = new Point(112, 300);
             EmailTb.Multiline = true;
             EmailTb.Name = "EmailTb";
-            EmailTb.PlaceholderText = "Password";
-            EmailTb.Size = new Size(176, 29);
-            EmailTb.TabIndex = 2;
+            EmailTb.PlaceholderText = "Email";
+            EmailTb.Size = new Size(176, 30);
+            EmailTb.TabIndex = 1;
+            EmailTb.UseSystemPasswordChar = true;
+            EmailTb.TextChanged += PasswordTb_TextChanged;
+            // 
+            // PasswordTb
+            // 
+            PasswordTb.Anchor = AnchorStyles.None;
+            PasswordTb.BackColor = Color.NavajoWhite;
+            PasswordTb.BorderStyle = BorderStyle.None;
+            PasswordTb.Location = new Point(111, 254);
+            PasswordTb.Multiline = true;
+            PasswordTb.Name = "PasswordTb";
+            PasswordTb.PlaceholderText = "Password";
+            PasswordTb.Size = new Size(176, 29);
+            PasswordTb.TabIndex = 2;
+            PasswordTb.TextChanged += EmailTb_TextChanged;
             // 
             // SignupBtn
             // 
@@ -96,12 +99,12 @@
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panel1.BackColor = Color.NavajoWhite;
             panel1.Controls.Add(label4);
-            panel1.Controls.Add(label3);
+            panel1.Controls.Add(passregTB);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(UserTb);
-            panel1.Controls.Add(PasswordTb);
             panel1.Controls.Add(EmailTb);
+            panel1.Controls.Add(PasswordTb);
             panel1.Controls.Add(linkLabel1);
             panel1.Controls.Add(SignupBtn);
             panel1.Location = new Point(307, 106);
@@ -110,17 +113,27 @@
             panel1.TabIndex = 8;
             panel1.Paint += panel1_Paint;
             // 
-            // label1
+            // label4
             // 
-            label1.Anchor = AnchorStyles.None;
-            label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.FromArgb(128, 64, 0);
-            label1.Location = new Point(99, 154);
-            label1.Name = "label1";
-            label1.Size = new Size(189, 31);
-            label1.TabIndex = 5;
-            label1.Text = "Grand Archive";
+            label4.Anchor = AnchorStyles.None;
+            label4.AutoSize = true;
+            label4.ForeColor = Color.FromArgb(128, 64, 0);
+            label4.Location = new Point(111, 315);
+            label4.Name = "label4";
+            label4.Size = new Size(192, 15);
+            label4.TabIndex = 8;
+            label4.Text = "_____________________________________";
+            // 
+            // passregTB
+            // 
+            passregTB.Anchor = AnchorStyles.None;
+            passregTB.AutoSize = true;
+            passregTB.ForeColor = Color.FromArgb(128, 64, 0);
+            passregTB.Location = new Point(111, 271);
+            passregTB.Name = "passregTB";
+            passregTB.Size = new Size(192, 15);
+            passregTB.TabIndex = 7;
+            passregTB.Text = "_____________________________________";
             // 
             // label2
             // 
@@ -133,27 +146,17 @@
             label2.TabIndex = 6;
             label2.Text = "_____________________________________";
             // 
-            // label3
+            // label1
             // 
-            label3.Anchor = AnchorStyles.None;
-            label3.AutoSize = true;
-            label3.ForeColor = Color.FromArgb(128, 64, 0);
-            label3.Location = new Point(111, 271);
-            label3.Name = "label3";
-            label3.Size = new Size(192, 15);
-            label3.TabIndex = 7;
-            label3.Text = "_____________________________________";
-            // 
-            // label4
-            // 
-            label4.Anchor = AnchorStyles.None;
-            label4.AutoSize = true;
-            label4.ForeColor = Color.FromArgb(128, 64, 0);
-            label4.Location = new Point(111, 315);
-            label4.Name = "label4";
-            label4.Size = new Size(192, 15);
-            label4.TabIndex = 8;
-            label4.Text = "_____________________________________";
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(128, 64, 0);
+            label1.Location = new Point(99, 154);
+            label1.Name = "label1";
+            label1.Size = new Size(189, 31);
+            label1.TabIndex = 5;
+            label1.Text = "Grand Archive";
             // 
             // Form1
             // 
@@ -172,14 +175,14 @@
         }
 
         private System.Windows.Forms.TextBox UserTb;
-        private System.Windows.Forms.TextBox PasswordTb;
         private System.Windows.Forms.TextBox EmailTb;
+        private System.Windows.Forms.TextBox PasswordTb;
         private System.Windows.Forms.Button SignupBtn;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private Panel panel1;
         private Label label1;
         private Label label4;
-        private Label label3;
+        private Label passregTB;
         private Label label2;
     }
 }
